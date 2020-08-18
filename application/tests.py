3c55d9d054fc617482c2b5c28d3b0d24e647fb9d
Mercy <aprilkasha42@gmail.com>
2020-08-18 21:33:46 +0300

    create a test for the profile model

--- application/tests.py
+++ application/tests.py
@@ -22,3 +22,17 @@ class NeighbourhoodTestClass(TestCase):
             self.new_neighbourhood.delete_neigborhood()
             neighborhood=Neighbourhood.objects.all()
             self.assertEqual(len(neighborhood),0)
+
+class ProfileTestClass(TestCase):
+
+    def setUp(self):
+
+        self.new_profile=Profile(bio='i love basketball')
+    # test for instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile,Profile))
+    # testing the save mothod
+    def test_save_profile(self):
+        self.new_profile.create_profile()
+        profile=Profile.objects.all()
+        self.assertTrue(len(profile)>0)

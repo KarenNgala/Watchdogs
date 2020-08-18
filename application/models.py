5e125742ed70fa21501def38aafd7269620a59f9
Mercy <aprilkasha42@gmail.com>
2020-08-18 21:31:50 +0300

    create model profile to display image,bio and user

--- application/models.py
+++ application/models.py
@@ -13,3 +13,18 @@ class Neighbourhood(models.Model):
 
     def delete_neigborhood(self):
         self.delete()
+
+
+class Profile(models.Model):
+    image=models.ImageField(default='default.jpg', upload_to='profile_pics')
+    bio=models.CharField(max_length=300)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def create_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()

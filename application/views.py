c684fed3833efc0e079c18b2013ba72f01133d44
Bryson69 <brysonmundia@gmail.com>
2020-08-20 09:31:07 +0300

    Add function

--- application/views.py
+++ application/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def neighbour(request):
+    return render(request, 'neighbourhood.html')

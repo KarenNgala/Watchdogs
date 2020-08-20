1be64fdb1ca346dcff266e4766fb6582b8ff5acb
Bryson69 <brysonmundia@gmail.com>
2020-08-20 09:30:48 +0300

    Add routing

--- application/urls.py
+++ application/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url(r'^$', views.home, name='home'),
+    url(r'^neighbour/', views.neighbour, name='neighbour'),
 ]
 
 if settings.DEBUG:

c38cc020c97455282a018dedef9aac10892f44b5
KarenNgala <ngalalalu@gmail.com>
2020-08-21 13:07:40 +0300

    Enable users to join hood on successful registration

--- application/urls.py
+++ application/urls.py
@@ -5,6 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url(r'^$', views.home, name='home'),
+    url(r'^accounts/register/complete/$', views.join, name='complete'),
+    url(r'^join/(\d+)/$', views.join_btn, name='join'),
 ]
 
 if settings.DEBUG:
